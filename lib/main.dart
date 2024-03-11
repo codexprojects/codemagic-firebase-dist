af416adb7b845092acfd1f755eee0fbf01d84452
İlke Yücel <yucel.ilke@gmail.com>
2024-03-11 23:31:22 +0200

    Add firebase sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:codemagic_ilke/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure plugin services are initialized
+  await Firebase.initializeApp( // Initialize Firebase
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
     return MaterialApp(
       title: 'Codemagic Rocks!',
       theme: ThemeData(
